4dbe169fa3104a1b50fd7a48882427ef49e3a08c
abdullahmohsen98 <abdo2dd@gmail.com>
2021-04-08 19:48:18 +0200

    Creating Author page with all features of (Create, Update, Delete, Edit)

---
+++ MyLibraryStore/Models/Repositors/BookRepository.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MyLibraryStore.Models.Repositors
+{
+    public class BookRepository : IBookstoreRepository<Book>
+    {
+        List<Book> books; //list of Book names books
+        public BookRepository() //constructor that will initialize that list
+        {
+            books = new List<Book>()
+            {
+                new Book
+                {
+                    Id=1, Title="C# Programming", Description="no description"
+                },
+                new Book
+                {
+                    Id=2, Title="Java Programming", Description="no description"
+                },
+                new Book
+                {
+                    Id=3, Title="Python Programming", Description="no description"
+                }
+            };
+        }
+        public void Add(Book entity)
+        {
+            books.Add(entity);
+        }
+
+        public void Delete(int id)
+        {
+            var book = books.SingleOrDefault(b => b.Id == id);
+            books.Remove(book);
+        }
+
+        public Book Find(int id)
+        {
+            var book = books.SingleOrDefault(b => b.Id == id);
+            return book;
+        }
+
+        public IList<Book> List()
+        {
+            return books;
+        }
+
+        public void Update(int id, Book newBook)
+        {
+            var book = Find(id);
+            book.Title = newBook.Title;
+            book.Description = newBook.Description;
+            book.Author = newBook.Author;
+        }
+    }
+}
---
+++ MyLibraryStore/Startup.cs
@@ -0,0 +1,46 @@
+﻿using MyLibraryStore.Models;
+using MyLibraryStore.Models.Repositors;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MyLibraryStore
+{
+    public class Startup
+    {
+        // This method gets called by the runtime. Use this method to add services to the container.
+        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
+        public void ConfigureServices(IServiceCollection services)
+        {
+            // here we will add our dependancies to services of ConfigureServices method so that they will be added to IOC container of asp.net core and be used as needed.
+           
+            //Adding the dependancy of mvc design pattern
+            services.AddMvc();
+
+            //Now we will add the dependancy of AuthorRepository:
+            //IBookstoreRepository is interface abstraction, AuthorRepository is a concrete class:
+            services.AddSingleton<IBookstoreRepository<Author>, AuthorRepository>();
+
+            //Now we will add the dependancy of BookRepository:
+            services.AddSingleton<IBookstoreRepository<Book>, BookRepository>();
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            app.UseStaticFiles();           //DefaultRoute for mvc
+            app.UseMvcWithDefaultRoute();
+        }
+
+
+    }
+}
